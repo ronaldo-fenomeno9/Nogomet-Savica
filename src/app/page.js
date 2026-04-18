1331fb624e8909555a8e3bcf81cc8791fc3bc7e5
ronaldo-fenomeno9 <rronaldo.fenomeno9@gmail.com>
2026-04-18 23:36:00 +0200

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,6 @@ import { useRouter } from 'next/navigation'
 
 export default function Home() {
   const router = useRouter()
-  useEffect(() => { router.replace('/statistika') }, [])
+  useEffect(() => { router.replace('/ljestvica') }, [])
   return null
 }
